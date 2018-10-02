c374dbec25c73cd19bd5c20807ef4cbc5fc54d96
Josh Braden <josbraden@gmail.com>
2018-10-02 16:17:17 -0500

    Added first iteration SQL script to update the systems table given a systems_recently.csv file

---
+++ update.sql
@@ -0,0 +1,23 @@
+--
+-- Script to update the systems table with new values from a systems_recently.csv
+-- Also requires the ed_import_schema.sql file
+--
+
+-- Add import tables
+-- Note: only the systems_import table is needed, but for simplicity sake keeping the three bundled in one file
+SOURCE ed_import_schema.sql;
+DROP TABLE factions_import,listings_import;
+
+-- Load CSV data
+LOAD DATA LOCAL INFILE 'systems_recently.csv' INTO TABLE systems_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+FLUSH TABLE systems_import;
+
+-- Merge imported data with systems table via DELETE+INSERT
+DELETE FROM systems WHERE eddb_id IN (SELECT eddb_id FROM systems_import);
+INSERT IGNORE INTO systems (eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id) SELECT eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id FROM systems_import;
+FLUSH TABLE systems;
+
+-- Cleanup import data
+DROP TABLE systems_import;
+
+-- TODO optimize systems table, rebuild primary index
