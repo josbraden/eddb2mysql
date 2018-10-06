aee865f2b52cd69c13809c0663e284cb8bf1b629
Josh Braden <josbraden@gmail.com>
2018-10-06 15:22:46 -0500

    Added download and parse comamnds for bodies data

--- getall.sh
+++ getall.sh
@@ -24,6 +24,9 @@ fi
 if [ ! -f ./stations.json ]; then
 	wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v5/stations.json | gzip -dc > stations.json
 fi
+if [ ! -f ./bodies.json ]; then
+	wget -O - -S --header="accept-encoding: gzip" https://www.edsm.net/dump/bodies.json | gzip -dc > bodies.json
+fi
 # Convert JSONs to CSVs
 # TODO get category from commodities.json into seperate table
 json2csv/bin/json2csv.js -f "id","name","category_id","average_price","is_rare","max_buy_price","max_sell_price","min_buy_price","min_sell_price","buy_price_lower_average","sell_price_upper_average","is_non_marketable","ed_id" -i commodities.json -o commodities.csv
@@ -33,3 +36,8 @@ json2csv/bin/json2csv.js -f "id","group_id","class","rating","price","weapon_mod
 # TODO missing fields on stations:
 # "import_commodities","export_commodities","prohibited_commodities","economies","selling_ships","selling_modules"
 json2csv/bin/json2csv.js -f "id","name","system_id","updated_at","max_landing_pad_size","distance_to_star","government_id","allegiance_id","state_id","type_id","type","has_blackmarket","has_market","has_refuel","has_repair","has_rearm","has_outfitting","has_shipyard","has_docking","has_commodities","shipyard_updated_at","outfitting_updated_at","market_updated_at","is_planetary","settlement_size_id","settlement_size","settlement_security_id","settlement_security","body_id","controlling_minor_faction_id" -i stations.json -o stations.csv
+# Missing fields in bodies:
+# "discovery","parents","belts"
+json2csv/bin/json2csv.js -f "id","id64","bodyId","name","type","subType","offset","distanceToArrival","isMainStar","isScoopable","age","spectralClass","luminosity","absoluteMagnitude","solarMasses","solarRadius","surfaceTemperature","orbitalPeriod","semiMajorAxis","orbitalEccentricity","orbitalInclination","argOfPeriapsis","rotationalPeriod","rotationalPeriodTidallyLocked","axialTilt","updateTime","systemId","systemId64","systemName" -i bodies.json -o bodies.csv
+# Cleaning bodies.json to save disk space
+rm -f bodies.json
