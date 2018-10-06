bb7a5069c9204bc8af5b8bc54ad667b65ae7a309
Josh Braden <josbraden@gmail.com>
2018-10-06 18:15:25 -0500

    Added bodies processing to update scripts

--- getupdate.sh
+++ getupdate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Script to just download systems_recently from eddb.io
-rm -f systems_recently.csv
+# Script to download delta data for import
+rm -f systems_recently.csv bodies7days.json bodies7days.csv
 wget -O - -S --header="accept-encoding: gzip" https://eddb.io/archive/v5/systems_recently.csv | gzip -dc > systems_recently.csv
-echo "File download complete, run update.sql to update the database."
+wget -O - -S --header="accept-encoding: gzip" https://www.edsm.net/dump/bodies7days.json | gzip -dc > bodies7days.json
+json2csv/bin/json2csv.js -f "id","id64","bodyId","name","type","subType","offset","distanceToArrival","isMainStar","isScoopable","age","spectralClass","luminosity","absoluteMagnitude","solarMasses","solarRadius","surfaceTemperature","orbitalPeriod","semiMajorAxis","orbitalEccentricity","orbitalInclination","argOfPeriapsis","rotationalPeriod","rotationalPeriodTidallyLocked","axialTilt","updateTime","systemId","systemId64","systemName" -i bodies7days.json -o bodies7days.csv
+echo "File downloads complete, run update.sql to update the database."
--- update.sql
+++ update.sql
@@ -1,27 +1,31 @@
 --
--- Script to update the systems table with new values from a systems_recently.csv
+-- Script to update the database with new data, using systems_recently.csv and bodies7days.json
 -- Also requires the ed_import_schema.sql file
 --
 
 -- Add import tables
--- Note: only the systems_import table is needed, but for simplicity sake keeping the three bundled in one file
 SOURCE ed_import_schema.sql;
-DROP TABLE factions_import,listings_import;
+DROP TABLE commodities_import,factions_import,listings_import,modules_import,stations_import;
 
 -- Load CSV data
 LOAD DATA LOCAL INFILE 'systems_recently.csv' INTO TABLE systems_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-FLUSH TABLE systems_import;
+LOAD DATA LOCAL INFILE 'bodies7days.csv' INTO TABLE bodies_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+FLUSH TABLE systems_import,bodies_import;
 
--- Merge imported data with systems table via DELETE+INSERT
+-- Merge imported data with tables via DELETE+INSERT
 DELETE FROM systems WHERE eddb_id IN (SELECT eddb_id FROM systems_import);
 INSERT IGNORE INTO systems(eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id) SELECT eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id FROM systems_import;
-FLUSH TABLE systems;
+DELETE FROM bodies WHERE eddb_id IN (SELECT eddb_id FROM bodies_import);
+INSERT IGNORE INTO bodies(eddb_id,id64,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId,systemId64,systemName) SELECT * FROM bodies_import;
+FLUSH TABLE systems,bodies;
 
 -- Cleanup import data
-DROP TABLE systems_import;
+DROP TABLE systems_import,bodies_import;
 
--- Re-index and optimize system table
+-- Re-index and optimize tables
 -- Probably only needs to happen every other month or so, but keeping on every update for now
 SET @newid=0;
 UPDATE systems SET id=(@newid:=@newid+1) ORDER BY id;
-OPTIMIZE TABLE systems;
+SET @newid=0;
+UPDATE bodies SET id=(@newid:=@newid+1) ORDER BY id;
+OPTIMIZE TABLE systems,bodies;
