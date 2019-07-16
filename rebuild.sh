93fa4ec75077886068300c12d26ef29a27fd4cc4
Josh Braden <josbraden@gmail.com>
2019-07-16 14:04:15 -0500

    Basic structure

--- rebuild.sh
+++ rebuild.sh
@@ -5,6 +5,20 @@
 # 2. Convert JSON files to CSV for MySQL LOAD using json2csv
 # 3. Run the mysql rebuild script
 #
+# New version of the script needs to:
+# 1. Import schema to MySQL
+# 2. Download/decompress/convert data from eddb/edsm
+# 3. Get data into database via mysqlimport
+# 3. Execute SQL code to build remaining tables
+#
+if [ ! -f ./mysqlinfo.txt ]; then
+	echo "MySQL info not found, exiting"
+	exit 1
+fi
+source ./mysqlinfo.txt
+echo "Importing Schema"
+mysql -u $mysqluser -p$mysqlpass -h $mysqlhost -D $mysqldb < ed_schema.sql
+mysql -u $mysqluser -p$mysqlpass -h $mysqlhost -D $mysqldb < ed_import_schema.sql
 # Downloads data files from web
 echo "Downloading data files"
 if [ ! -f ./factions.csv ]; then
@@ -36,10 +50,13 @@ if [ ! -f ./bodies.csv ]; then
 fi
 echo "File download complete."
 # Load data into MySQL
-if [ ! -f ./mysqlinfo.txt ]; then
-	echo "MySQL info not found, please run rebuild.sql manually to build the database"
-	echo "e.g.: $ mysql -u root -p -h localhost ed < rebuild.sql"
-else
-	source ./mysqlinfo.txt
-	mysql -u $mysqluser -p$mysqlpass -h $mysqlhost -D $mysqldb < rebuild.sql
-fi
+# Import table loads
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost $mysqldb systems.csv
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost $mysqldb stations.csv
+# Direct table loads
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,name,updated_at,government_id,allegiance_id,state_id,home_system_id,is_player_faction $mysqldb factions.csv
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,station_id,commodity_id,supply,supply_bracket,buy_price,sell_price,demand,demand_bracket,collected_at $mysqldb listings.csv
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,name,category_id,average_price,is_rare,max_buy_price,max_sell_price,min_buy_price,min_sell_price,buy_price_lower_average,sell_price_upper_average,is_non_marketable,ed_id $mysqldb commodities.csv
+mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,group_id,class,rating,price,weapon_mode,missile_type,name,belongs_to,ed_id,ed_symbol,ship $mysqldb modules.csv
+# bodies TODO
+#mysqlimport --local -u $mysqluser -p$mysqlpass -h $mysqlhost -c eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId $mysqldb bodies.csv
