36bb06d7f078e3b4be1edea3610fc4cbdac78dbb
Josh Braden <josbraden@gmail.com>
2019-07-16 10:43:49 -0500

    Create bodies_import, import new datafile to it and delete/select/insert/drop

--- updateweekly.sql
+++ updateweekly.sql
@@ -7,12 +7,13 @@
 
 -- Add import tables
 SOURCE ed_import_schema.sql;
+CREATE TABLE bodies_import SELECT * FROM bodies LIMIT 0;
 DROP TABLE stations_import;
 
 -- Load CSV data
 LOAD DATA LOCAL INFILE 'systems_recently.csv' INTO TABLE systems_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-LOAD DATA LOCAL INFILE 'bodies7days.csv' INTO TABLE bodies_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-FLUSH TABLE systems_import,listings_import,bodies_import;
+LOAD DATA LOCAL INFILE 'bodies7days.csv' INTO TABLE bodies_import(eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
+FLUSH TABLE systems_import,bodies_import;
 
 -- listings table rebuild
 TRUNCATE TABLE listings;
@@ -23,11 +24,11 @@ FLUSH TABLE listings;
 DELETE FROM systems WHERE eddb_id IN (SELECT eddb_id FROM systems_import);
 INSERT IGNORE INTO systems(eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id) SELECT eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id FROM systems_import;
 DELETE FROM bodies WHERE eddb_id IN (SELECT eddb_id FROM bodies_import);
-INSERT IGNORE INTO bodies(eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) SELECT * FROM bodies_import;
+INSERT IGNORE INTO bodies(eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) SELECT (eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) FROM bodies_import;
 FLUSH TABLE systems,bodies;
 
 -- Cleanup import data
-DROP TABLE systems_import;
+DROP TABLE bodies_import,systems_import;
 
 -- Re-index and optimize tables
 -- Probably only needs to happen every other month or so, but keeping on every update for now
