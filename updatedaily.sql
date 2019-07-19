9374c786642e8d1f5708fad4c92f568b38111ebb
Josh Braden <josbraden@gmail.com>
2019-07-19 14:41:21 -0500

    Add some print statements to the daily/weekly update scripts

--- updatedaily.sql
+++ updatedaily.sql
@@ -1,5 +1,7 @@
 -- Script to do daily updates to the database
 -- Currently only the market listings table makes sense to update on such an aggressive frequency
+SELECT 'Updating listings table' AS '';
 TRUNCATE TABLE listings;
 LOAD DATA LOCAL INFILE 'listings.csv' INTO TABLE listings FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (eddb_id,station_id,commodity_id,supply,supply_bracket,buy_price,sell_price,demand,demand_bracket,collected_at);
 FLUSH TABLE listings;
+SELECT 'Done updating listings table' AS '';
--- updateweekly.sql
+++ updateweekly.sql
@@ -8,21 +8,33 @@ SOURCE ed_import_schema.sql;
 CREATE TEMPORARY TABLE bodies_import SELECT * FROM bodies LIMIT 0;
 DROP TABLE stations_import;
 
+-- listings table rebuild
+SELECT 'Updating listings table' AS '';
+TRUNCATE TABLE listings;
+LOAD DATA LOCAL INFILE 'listings.csv' INTO TABLE listings FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (eddb_id,station_id,commodity_id,supply,supply_bracket,buy_price,sell_price,demand,demand_bracket,collected_at);
+FLUSH TABLE listings;
+SELECT 'Done updating listings table' AS '';
+
 -- Load CSV data
 LOAD DATA LOCAL INFILE 'systems_recently.csv' INTO TABLE systems_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
 LOAD DATA LOCAL INFILE 'bodies7days.csv' INTO TABLE bodies_import FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId);
 FLUSH TABLE systems_import,bodies_import;
-
--- listings table rebuild
-TRUNCATE TABLE listings;
-LOAD DATA LOCAL INFILE 'listings.csv' INTO TABLE listings FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES (eddb_id,station_id,commodity_id,supply,supply_bracket,buy_price,sell_price,demand,demand_bracket,collected_at);
-FLUSH TABLE listings;
+SELECT COUNT(*) FROM systems_import AS 'Imported systems Count';
+SELECT COUNT(*) FROM bodies_import AS 'Imported bodies Count';
 
 -- Merge imported data with tables via DELETE+INSERT
+-- systems
+SELECT COUNT(*) FROM systems AS 'Old systems Count';
+SELECT COUNT(*) FROM systems WHERE eddb_id IN (SELECT eddb_id FROM systems_import) AS 'Deleting systems';
 DELETE FROM systems WHERE eddb_id IN (SELECT eddb_id FROM systems_import);
 INSERT IGNORE INTO systems(eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id) SELECT eddb_id,edsm_id,name,x,y,z,population,is_populated,government_id,allegiance_id,state_id,security_id,primary_economy_id,power,power_state_id,needs_permit,updated_at,controlling_minor_faction_id,reserve_type_id FROM systems_import;
+SELECT COUNT(*) FROM systems AS 'New systems Count';
+-- bodies
+SELECT COUNT(*) FROM bodies AS 'Old bodies Count';
+SELECT COUNT(*) FROM bodies WHERE eddb_id IN (SELECT eddb_id FROM bodies_import) AS 'Deleting bodies';
 DELETE FROM bodies WHERE eddb_id IN (SELECT eddb_id FROM bodies_import);
 INSERT IGNORE INTO bodies(eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) SELECT (eddb_id,bodyId,name,type,subType,offset,distanceToArrival,isMainStar,isScoopable,age,spectralClass,luminosity,absoluteMagnitude,solarMasses,solarRadius,surfaceTemperature,orbitalPeriod,semiMajorAxis,orbitalEccentricity,orbitalInclination,argOfPeriapsis,rotationalPeriod,rotationalPeriodTidallyLocked,axialTilt,updateTime,systemId) FROM bodies_import;
+SELECT COUNT(*) FROM bodies AS 'New bodies Count';
 FLUSH TABLE systems,bodies;
 
 -- Cleanup import data
